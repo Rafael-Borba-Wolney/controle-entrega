9eb911767d3a30f63755a2619daf28b10ef5463d
Rafael-Borba-Wolney <rborba2026@gmail.com>
2026-04-04 22:09:25 -0300

    cria estrutura inicial do projeto

---
+++ src/main/java/com/example/controleentrega/ControleEntregaApplication.java
@@ -0,0 +1,12 @@
+package com.example.controleentrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEntregaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleEntregaApplication.class, args);
+    }
+}
